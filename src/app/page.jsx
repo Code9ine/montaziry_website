d5ac3fc7876772faea5021ab4e944508cb53d564
Abdulah Qasemi <abdullahqasemi2019@gmail.com>
2024-04-27 23:47:18 +0430

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,25 @@
-import { Button } from "@mantine/core";
-import Link from "next/link";
+import { AspectRatio, Container } from "@mantine/core";
+import Image from "next/image";
 
 const Home = () => {
   return (
-    <div>
-      <div>In the name of Allah</div>
-      <div>Started By Code9ine Company</div>
-      <Link href={"/admin"}>
-        <Button>admin</Button>
-      </Link>
-    </div>
+    <Container size={"lg"}>
+      <div className="top-landing">
+        <AspectRatio ratio={1080 / 400}>
+          <Image src={"/images/top-bg.jpg"} alt={""} fill={true} />
+        </AspectRatio>
+        <div className="navbar flex items-center h-[80px]  px-4">
+          <Image
+            src={"/images/profile.jpeg"}
+            height={160}
+            width={160}
+            alt="profile"
+            className="rounded-full translate-y-[-40px] border-[6px]"
+          />
+          <h1 className="text-2xl font-semibold ms-4">Dr. Montazeri</h1>
+        </div>
+      </div>
+    </Container>
   );
 };
 
